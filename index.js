4dca934995a44cdffd9972a7fac81a79593786fd
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-08 19:12:31 +0300

    commit tree 4cc97e1616ad694ee06f2804d710c915aecdd5ba

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+const employee = {
+  name: "Cain",
+  streetAddress: "00100-Bethlehem",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  return {
+    ...obj,
+    [key]: value,
+  };
+
+  //   const newObj = { ...obj };
+  //   newObj[key] = value;
+  //   return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+  obj[key] = value;
+  return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+  //does not mutate original employee object
+  const newObj = { ...obj };
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+  //mutates original employee object
+  delete obj[key];
+  return obj;
+}
